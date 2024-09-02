0300112d78b394f3e91c46c29da890bbd86c73e2
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-02 16:00:35 -0500

    More wam

--- bin/smol.ts
+++ bin/smol.ts
@@ -5,10 +5,13 @@ import { SmolStack } from "../lib/smol-stack";
 
 const app = new cdk.App();
 
+const DEFAULT_MEMORY_LIMIT = 8096;
 new SmolStack(app, "SmolStack", {
   env: {
     account: "065495811055",
     region: "us-east-1",
   },
-  memoryLimit: parseInt(process.env.MEMORY_LIMIT || "2048") || 2048,
+  memoryLimit:
+    parseInt(process.env.MEMORY_LIMIT || `${DEFAULT_MEMORY_LIMIT}`) ||
+    DEFAULT_MEMORY_LIMIT,
 });
